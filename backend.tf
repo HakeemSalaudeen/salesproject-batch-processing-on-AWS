34276200edfddeee408142578e8c371278e6f848
Hakeem Oluwatobi <Hakeemsalaudeen01@gmail.com>
2025-04-02 11:24:52 +0100

    updated

--- backend.tf
+++
@@ -1,8 +0,0 @@
-# create terraform state file s3 bucket
-terraform {
-  backend "s3" {
-    bucket = "statefile-backend-bucket"
-    region = "eu-central-1"
-    key    = "ty-salesproject/salesproject/terraform.tfstate"
-  }
-}
